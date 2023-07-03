04433333a1599577e9cf018fa11cfd2ef3880d4a
sappy666 <macarenaramdohr@gmail.com>
2023-07-03 18:32:50 -0400

    servicios arreglos

--- src/components/ServiciosComp.vue
+++ src/components/ServiciosComp.vue
@@ -9,65 +9,66 @@
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
                <div class="service-item  position-relative">
                   <div class="icon">
-                     <i class="fa-solid fa-mountain-city"></i>
+                     <img src="../assets/iconos/library.png" alt="" srcset="">
                   </div>
                   <h3>Contratación Pública</h3>
                   <p>Nuestro equipo está conformado por un grupo de abogados e ingenieros especializados en Contratación Pública. A su vez contamos con vasta experiencia en portal mercadopublico.cl y chilecompra.cl.</p>
-                  <a href="service-details.html" class="readmore stretched-link">Leer más<i class="bi bi-arrow-right"></i></a>
+                  <router-link class="readmore" to="/AreasView">Leer más</router-link>
                </div>
             </div>
             <!-- End Service Item -->
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
                <div class="service-item position-relative">
                   <div class="icon">
-                     <i class="fa-solid fa-arrow-up-from-ground-water"></i>
+                     <img src="../assets/iconos/socialize.png" alt="" srcset="">
                   </div>
                   <h3>Asesoría en postulación</h3>
                   <p>Nuestro plan te ayuda a evitar errores comunes en las licitaciones públicas, optimizando tus posibilidades de adjudicación..</p>
-                  <a href="service-details.html" class="readmore stretched-link">Leer más<i class="bi bi-arrow-right"></i></a>
+                  <router-link class="readmore" to="/AreasView">Leer más</router-link>
                </div>
             </div>
             <!-- End Service Item -->
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
                <div class="service-item position-relative">
                   <div class="icon">
-                     <i class="fa-solid fa-compass-drafting"></i>
+                     <img src="../assets/iconos/law.png" alt="" srcset="">
                   </div>
                   <h3>Defensa Proveedor/Contratista</h3>
                   <p>Especializados en la defensa de contratistas y proveedores del Estado a través del Mercado Público, protegemos sus derechos en una relación comercial desigual, brindando conocimientos técnicos y jurídicos especializados.</p>
-                  <a href="service-details.html" class="readmore stretched-link">Leer más<i class="bi bi-arrow-right"></i></a>
+                  <router-link class="readmore" to="/AreasView">Leer más</router-link>
                </div>
             </div>
             <!-- End Service Item -->
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
                <div class="service-item position-relative">
                   <div class="icon">
-                     <i class="fa-solid fa-trowel-bricks"></i>
+                     <img src="../assets/iconos/gavel.png" alt="" srcset="">
                   </div>
                   <h3>Tribunal Contratación Pública</h3>
                   <p>Estudio jurídico especializado en tramitación eficiente y exitosa ante el Tribunal de Contratación Pública. Nuestras oficinas ubicadas cerca del Tribunal garantizan un proceso judicial fluido y ágil.</p>
-                  <a href="service-details.html" class="readmore stretched-link">Leer más<i class="bi bi-arrow-right"></i></a>
+                  <router-link class="readmore" to="/AreasView">Leer más</router-link>
                </div>
             </div>
             <!-- End Service Item -->
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
                <div class="service-item position-relative">
                   <div class="icon">
-                     <i class="fa-solid fa-helmet-safety"></i>
+                     <img src="../assets/iconos/diagram.png" alt="" srcset="">
                   </div>
                   <h3>Gestión de Negocios</h3>
-                  <p>Brindamos asesoría continua a personas jurídicas y naturales, ofreciendo servicios integrales en negociación, puesta en marcha, desarrollo y cierre de negocios. Creamos contratos personalizados para satisfacer las necesidades de cada cliente. Además, proporcionamos informes con análisis jurídicos detallados sobre operaciones comerciales y más.</p>
-                  <router-link class="nav-link" to="/AreasView">Leer más</router-link>               </div>
+                  <p>Brindamos asesoría continua a personas jurídicas y naturales, ofreciendo servicios integrales en negociación, puesta en marcha, desarrollo y cierre de negocios. Creamos contratos personalizados para satisfacer las necesidades de cada cliente.</p>
+                  <router-link class="readmore" to="/AreasView">Leer más</router-link>
+               </div>
             </div>
             <!-- End Service Item -->
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
                <div class="service-item position-relative">
                   <div class="icon">
-                     <i class="fa-solid fa-arrow-up-from-ground-water"></i>
+                     <img src="../assets/iconos/construction-and-tools.png" alt="" srcset="">
                   </div>
                   <h3>Derecho Laboral</h3>
                   <p>Nuestros abogados son expertos en litigación laboral ejerciendo día a día la defensa frente a los distintos Tribunales del país de los intereses de su representado.</p>
-                  <a href="service-details.html" class="readmore stretched-link">Leer más<i class="bi bi-arrow-right"></i></a>
+                  <router-link class="readmore" to="/AreasView">Leer más</router-link>
                </div>
             </div>
             <!-- End Service Item -->
@@ -87,13 +88,13 @@
    background: #fff;
    height: 100%;
    }
-   .services .service-item .icon {
+   .services .service-item .icon, .services .service-item img {
    width: 48px;
    height: 48px;
    position: relative;
    margin-bottom: 50px;
    }
-   .services .service-item .icon i {
+   .services .service-item .icon i, .services .service-item .icon img{
    color: var(--color-secondary);
    font-size: 40px;
    transition: ease-in-out 0.3s;
@@ -135,7 +136,7 @@
    color: var(--color-primary);
    }
    .services .service-item:hover .icon:before {
-   background: var(--color-primary);
+   background: rgba(155, 159, 213, 0.389);
    }
    .services .service-item:hover h3 {
    border-color: var(--color-primary);
