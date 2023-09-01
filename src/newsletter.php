60e056e3e97f80855a1e41cfb39c806a4c95cf87
JJ1312 <yeiyeithirteen@gmail.com>
2023-08-31 22:59:16 -0400

    Cambios

--- src/newsletter.php
+++ src/newsletter.php
@@ -7,7 +7,7 @@ $response = $client->request('POST', 'https://api.brevo.com/v3/contacts', [
   'body' => '{"updateEnabled":false,"email":"'.$email.'","emailBlacklisted":true,"smsBlacklisted":false,"listIds":[3]}',
   'headers' => [
     'accept' => 'application/json',
-    'api-key' => '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 ',
+    'api-key' => '',
     'content-type' => 'application/json',
   ],
 ]);
