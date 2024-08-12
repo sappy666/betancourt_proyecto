6e11e75964547e96ea6430ee7ede83c819e6dba4
JJ <yeiyeithirteen@gmail.com>
2024-08-12 15:23:32 -0400

    Duplica imagenes para crear ciclo infinito de logos

--- src/components/ClientesComp.vue
+++ src/components/ClientesComp.vue
@@ -324,8 +324,620 @@
               height="60"
               loading="lazy"
             />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/47.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/48.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/49.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/50.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/51.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/52.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/53.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/54.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/55.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/56.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/57.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/58.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/59.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/60.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/61.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/62.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/63.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/64.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/65.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/66.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/67.png"
+              height="60"
+              loading="lazy"
+            />
           </li>
         </ul>
+        <ul class="logos-slide">
+          <li class="">
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/1.jpg"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/2.jpg"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+             
+              class="d-block mx-auto"
+              src="../assets/clientes/3.jpg"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/4.jpg"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/5.jpg"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/6.jpg"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/7.jpg"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/8.jpg"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/9.jpg"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/10.jpg"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/11.jpg"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/12.jpg"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto"
+              src="../assets/clientes/13.jpg"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/14.jpg"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/15.jpg"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/16.jpg"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/17.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/18.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/19.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/20.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/22.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/23.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/24.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/25.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/26.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/27.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/28.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/29.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/30.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/31.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/32.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/33.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/34.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/35.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/36.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/37.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/38.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/39.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/40.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/41.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/42.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/43.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/44.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/45.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/46.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/47.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/48.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/49.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/50.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/51.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/52.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/53.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/54.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/55.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/56.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/57.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/58.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/59.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/60.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/61.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/62.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/63.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/64.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/65.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+          <li class="">
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/66.png"
+              height="60"
+              loading="lazy"
+            />
+            <img
+              class="d-block mx-auto "
+              src="../assets/clientes/67.png"
+              height="60"
+              loading="lazy"
+            />
+          </li>
+        </ul>
+
       </div>
   </section>
 </template>
@@ -379,16 +991,16 @@ export default {
   white-space: nowrap;
   padding: 0;
 }
-.logos .logos-slide{
+.logos-slide{
     padding: 0;
     list-style: none;
     margin: 0;
-    animation: 20s slide infinite linear;
-    animation-play-state: running;
-
-      }
-.logos .logos-slide:hover{
-      animation-play-state: paused
+    animation: 50s slide infinite linear;
+    /* animation-play-state: running; */
+    display: inline-block;
+ }
+.logos-slide:hover{
+      /* animation-play-state: paused */
 }
 
 li{
