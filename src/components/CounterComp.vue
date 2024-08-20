1702a3e986d974020dd25648b0785ed409b6836f
JJ <yeiyeithirteen@gmail.com>
2024-08-19 20:33:40 -0400

    Cambio 12 a 13 annos de experiencia

--- src/components/CounterComp.vue
+++ src/components/CounterComp.vue
@@ -38,7 +38,7 @@
         </div>
         <div class="col-lg-3 col-md-6">
           <div class="d-flex align-items-center">
-            <h6 id="count-annos" class="h1 mb-0">+12</h6>
+            <h6 id="count-annos" class="h1 mb-0">+13</h6>
             <p
               v-if="this.lang == 'es'"
               class="ms-3 flex-grow-1 text-uppercase mb-0"
